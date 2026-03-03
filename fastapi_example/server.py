63a873e56b8d15f6e0271c5019f5f91d08168446
vidhi-hestabit <vidhia.hestabit@gmail.com>
2026-03-03 17:50:32 +0530

    proxy header true

--- fastapi_example/server.py
+++ fastapi_example/server.py
@@ -30,4 +30,4 @@ PORT = int(os.environ.get("PORT", 10000))
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    uvicorn.run(app, host="0.0.0.0", port=PORT, proxy_headers=True)
